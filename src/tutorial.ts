62162dd7b28ae9cb2b08e42e891dbe4bc1a405e0
salaudeen olanrewaju <olanrewajulanzy@gmail.com>
2024-04-25 02:24:28 +0100

    smilga typescript course

---
+++ src/tutorial.ts
@@ -0,0 +1,24 @@
+// // type annotation
+// let awesomeName: string = "shakeAndBake";
+// awesomeName = "something";
+// awesomeName = awesomeName.toUpperCase();
+// console.log(awesomeName);
+
+// let amount: number = 20;
+// amount = 12 - 1;
+
+// let isAwesome: boolean = true;
+// isAwesome = false;
+// isAwesome = "shakeAndBake"
+
+// let greetings: string = "Hello Lanre";
+// greetings = greetings.toLowerCase();
+// console.log(greetings);
+
+// let age: number = 25;
+// age = age + 5;
+// console.log(age);
+
+// let isAdult: boolean = age >= 18;
+// isAdult = !isAdult;
+// console.log(isAdult);
