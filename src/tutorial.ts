176bd9544c21a3f7390e138b8f6da6fff5ad7a38
salaudeen olanrewaju <olanrewajulanzy@gmail.com>
2024-04-29 23:03:30 +0100

    type aliases

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -56,12 +56,12 @@
 // orderStatus = "shipped";
 // orderStatus = "cancel";
 
-let prices: number[] = [100, 75, 42];
-prices.push(45);
-// prices.push("34")
+// let prices: number[] = [100, 75, 42];
+// prices.push(45);
+// // prices.push("34")
 
-let fruit: string[] = ["apple", "orange"];
-fruit.push("guava");
+// let fruit: string[] = ["apple", "orange"];
+// fruit.push("guava");
 // fruit.push(88)
 
 // let randomValues:[] = [];
@@ -110,4 +110,141 @@ fruit.push("guava");
 // sayHi("lanre");
 // sayHi(3);
 
-function calculateDiscount(price);
+// function calculateDiscount(price: number): number {
+//   const hasDiscount = true;
+//   if (hasDiscount) {
+//     return price;
+//   }
+//   return price * 0.9;
+// }
+
+// const finalPrice = calculateDiscount(200);
+
+// function addThree(number: any) {
+//   let anotherNumber: number = 3;
+//   return number + anotherNumber;
+// }
+// const result = addThree(3);
+// const someValue = result;
+
+// const names: string[] = ["lanre", "Ayo", "Azeezat", "jim"];
+
+// function isNameInlist(name: string): boolean {
+//   return names.includes(name);
+// }
+
+// let nameToCheck = "peter";
+
+// if (isNameInlist(nameToCheck)) {
+//   console.log(`${nameToCheck} is in the list`);
+// } else {
+//   console.log(`${nameToCheck} is not on the list`);
+// }
+
+// function calculatePrice(price: number, discount?: number): number {
+//   return price - (discount || 0);
+// }
+
+// let priceAfterDiscount = calculatePrice(100, 20);
+
+// function calculateScore(
+//   initialScore: number,
+//   penaltyPoints: number = 0
+// ): number {
+//   return initialScore - penaltyPoints;
+// }
+
+// let scoreAfterPenalty = calculateScore(100, 20);
+
+// let scoreWithoutPenalty = calculateScore(300);
+
+// function sum(message: string, ...numbers: number[]): string {
+//   const reduced = numbers.reduce((a, c) => a + c, 0);
+//   return ` ${message} ${reduced}`;
+// }
+
+// let result = sum(" The total is", 1, 2, 3, 4, 5);
+// console.log(result);
+
+// function logMessage(message: string): void {
+//   console.log(message);
+//   //   return "hello world";
+// }
+
+// logMessage("Hello, Typescript");
+
+// function processInput(input: string | number) {
+//   if (typeof input === "string") {
+//     console.log(`Input is of string type:${input.toUpperCase()}`);
+//   } else if (typeof input === "number") {
+//     console.log(`Input is of number type ${input}`);
+//   } else {
+//     console.log(`Input is neither of string type nor number type `);
+//   }
+// }
+
+// processInput(45);
+// processInput("lanre");
+// processInput(true);
+
+// function createEmployee({ id }: { id: number }): {
+//   id: number;
+//   isActive: boolean;
+// } {
+//   return { id, isActive: id % 2 === 0 };
+// }
+
+// const first = createEmployee({ id: 1 });
+// const second = createEmployee({ id: 2 });
+// console.log(first, second);
+
+// function createStudent(student: { id: number; name: string }): void {
+//   console.log(`Welcome to the course ${student.name.toUpperCase()}`);
+// }
+
+// const newStudent = {
+//   id: 5,
+//   name: "Ridwan",
+// };
+
+// createStudent(newStudent);
+
+// function processData(
+//   input: string | number,
+//   config: { reverse: boolean } = { reverse: false }
+// ): string | number {
+//   if (typeof input === "number") {
+//     return input * input;
+//   } else {
+//     return config.reverse
+//       ? input.toUpperCase().split("").reverse().join("")
+//       : input.toUpperCase();
+//   }
+// }
+
+// console.log(processData(10));
+// console.log(processData("hello"));
+// console.log(processData("hello", { reverse: true }));
+type user = { id: number; name: string; isActive: boolean };
+const john: user = {
+  id: 1,
+  name: "john",
+  isActive: true,
+};
+
+const susan: user = {
+  id: 1,
+  name: "susan",
+  isActive: false,
+};
+
+function createUser(user: user): user {
+  console.log(`Hello there ${user.name.toUpperCase()}!!!`);
+  return user;
+}
+
+// const john : { id: number; name: string; isActive: boolean} ={
+//   id:1,
+//   name: "john",
+//   isActive: true,
+// };
